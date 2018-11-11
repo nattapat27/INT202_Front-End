e65b0ab54caf43b454a425e14a879ce95f9fa09d
POPPOP <virada.weera@mail.kmutt.ac.th>
2018-11-11 23:44:18 +0700

    [ADD] Vuex ตัวอย่าง

--- src/store.js
+++ src/store.js
@@ -2,15 +2,25 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 
 Vue.use(Vuex)
-
 export default new Vuex.Store({
-  state: {
+  state: { //ตัวแปรทั้งสถานะที่ระบบจะเห้น
+    isCurrentCheckout: true
 
   },
   mutations: {
-
+      setCurentCheckoutStatus: function(state, status){
+        state.isCurrentCheckout = status
+      }
   },
   actions: {
+    setCurentCheckoutStatus: function ({commit}, status){
+      commit('setCurentCheckoutStatus',status)
+    }
+  },
+  getters:{
+    getCurrentCheckoutStatus: function(state){
+      return state.isCurrentCheckout
 
+    }
   }
 })
