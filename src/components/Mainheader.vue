2d8741f666c2b2cf6f6d25f4780674176cd061f9
Unknown <wdrdres3qew5ts21@gmail.com>
2018-11-13 23:38:40 +0700

    Auto stash before merge of "master" and "origin/master"

--- src/components/Mainheader.vue
+++ src/components/Mainheader.vue
@@ -1,14 +1,32 @@
   <template>
+<<<<<<< Updated upstream
     <v-toolbar app color="teal lighten-3">
       <v-toolbar-title class="headline text-uppercase">
        <div>
+=======
+
+    <v-toolbar app color="teal lighten-3">
+      <v-toolbar-title class="headline text-uppercase"  >
+       <div>
+
+>>>>>>> Stashed changes
     <v-btn color="success">All Categories</v-btn>
     <v-btn color="error">Best Seller</v-btn>
     <v-btn color="warning">Super deal</v-btn>
     <v-btn color="info">Event coupon</v-btn>
+<<<<<<< Updated upstream
       </div>
       </v-toolbar-title>
       <v-spacer>
+=======
+
+      </div>
+      </v-toolbar-title>
+      <v-spacer>
+
+         </v-spacer>
+      <v-spacer>
+>>>>>>> Stashed changes
           <v-text-field
         append-icon="mic"
         class="mx-3"
@@ -26,8 +44,14 @@
     </v-toolbar>
 
    </template>
+<<<<<<< Updated upstream
 <script>
 export default {
 
 }
+=======
+
+<script>
+
+>>>>>>> Stashed changes
 </script>
