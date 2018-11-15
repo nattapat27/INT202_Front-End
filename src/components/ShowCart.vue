e71dccbaee4d1cc8c12ac0c330afe7c054e2764f
kanwara <fonfonvfc@gmail.com>
2018-11-16 00:24:46 +0700

    [FIX]ShowCart

--- src/components/ShowCart.vue
+++ src/components/ShowCart.vue
@@ -1,22 +1,69 @@
 <template>
     <div>
-        <center><h1 class="header">Added To Cart</h1></center>
+        <!-- <center><h1 class="header">Added To Cart</h1></center> -->
 
-        <div class="ImageProductInCart">
-            <img src="https://bit.ly/2FlUDt7" style="width: 200px" height="200px">
-        
+        <div class="imageProduct">
+            <img src="https://bit.ly/2FlUDt7" style="width: 250px" height="250px">
+        </div>
 
-        <p class="titleProduct">Ralph Breaks the Internet
-            <br> Mini ''Tsum Tsum'' Collection</p><br><p class="subtitle">Qty: 1 at $5.95</p>
+        <div><p class="titleProduct">Ralph Breaks the Internet
+            <br> Mini ''Tsum Tsum'' Collection</p><br>
+            <p class="subtitle">$5.95
+              <br>Quanlity :
+                      <b-dropdown id="ddown-sm-split" size="sm" split text="1" class="m-2">
+                          <b-dropdown-item-button>1</b-dropdown-item-button>
+                          <b-dropdown-item-button>2</b-dropdown-item-button>
+                          <b-dropdown-item-button>3</b-dropdown-item-button>
+                          <b-dropdown-item-button>4</b-dropdown-item-button>
+                          <b-dropdown-item-button>5</b-dropdown-item-button>
+                          <b-dropdown-item-button>6</b-dropdown-item-button>
+                          <b-dropdown-item-button>7</b-dropdown-item-button>
+                          <b-dropdown-item-button>8</b-dropdown-item-button>
+                          <b-dropdown-item-button>9</b-dropdown-item-button>
+                          <b-dropdown-item-button>10</b-dropdown-item-button>
+                      </b-dropdown>
+            </p>
 
 <br>
   
-            <v-btn color="#D1D0CE" large >Continue Shopping</v-btn>
+            
+          <div class="Button">
+            <v-btn class="buttonBuyNow" color="#5670BA" large >Buy Now</v-btn>   
+
+          <br><br><br><br>
+         
           
-            <v-btn color="#5cb3ff" large>Continue Shopping</v-btn></div>
+          </div>
+          <center><div> <p class="underline">__________________________________________________________________________________________________________________________________________________</p>
+          </div></center>
+
+
+
+                  <div class="ProductDetail">
+                        <p class="ProductDetailTitle">Product Details</p>
+                        <br>
+                  <p class="subheader">Item No. 1234041281402P</p>
+                      <p class="bodyText">Retro video game bad guy Wreck-It Ralph will make a great addition to your <br>
+                      mini “Tsum Tsum”  plush collection. Inspired by Ralph Breaks the Internet, <br>
+                      this adorable plush is ready to wreck it!
+                      </p>
+                     <p class="subheader">Details</p>
+                      <p class="bodyText">- Mini “Tsum Tsum” plush <br>
+                      - Embroidered features <br>
+                      - Soft, squeezable fill with beans in belly <br>
+                      - Fuzzy plush texturing <br>
+                      - Part of our Ralph Breaks the Internet Mini “Tsum Tsum” Plush Collection</p>
+
+                       <p class="subheader">The bare necessities</p>
+                      <p class="bodyText">- Polyester/ plastic pellets
+                      - 3 1/2” L
+                      - Imported
+                      </p>
+                  </div>
+        </div>
           
     </div>
-
+              
     
 </template>
 
@@ -26,28 +73,69 @@
 </script>
 
 <style scoped>
-.ImageProductInCart {
-  margin-left: 35%;
+.imageProduct {
+  margin-left: 12%;
 }
 .header {
   margin-top: 2.5%;
 }
 .titleProduct {
+  color: #5670BA;
   font-size: 16px;
   font-weight: bold;
   float: right;
-  margin-top: 5%;
-  margin-right: 40%;
+  margin-top: -15%; 
+  margin-right: 50%; 
 }
 
 .subtitle {
+  color: #979BA9;
   font-size: 12px;
   float: right;
-  margin-right: 54%;
-  margin-top: -10%;
-}
-
+  margin-right: 55%;
+  margin-top: -11%;
+} 
 /* .buttonContinueShopping {
   
 } */
+
+.Button{
+  float: right;
+  margin-right: 38%;
+  margin-top: -6%;
+}
+.buttonBuyNow{
+  color: #ffffff;
+
+  /* margin-right: 38%;
+  margin-top: -6%; */
+}
+
+
+
+.underline{
+  color: #979BA9;
+}
+
+.ProductDetailTitle{
+  color: #979BA9;
+  font-size: 16px;
+  font-weight: bold;
+  color: #979BA9;
+  margin-top: 2%;
+  margin-left: 18%;
+}
+
+.subheader{
+  font-weight: bold;
+  font-size: 12px;
+  margin-left: 18%;
+  color: #979BA9;
+}
+
+.bodyText{
+  margin-left: 18%;
+  color: #979BA9;
+}
+
 </style>
