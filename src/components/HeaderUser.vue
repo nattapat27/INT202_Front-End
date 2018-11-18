bcd5c8e22b748858ef0cf4c75da5f3d893d8a8c6
POPPOP <virada.weera@mail.kmutt.ac.th>
2018-11-18 22:40:30 +0700

    [EDIT] Main HeaderUer component

--- src/components/HeaderUser.vue
+++ src/components/HeaderUser.vue
@@ -1,7 +1,7 @@
 <template>
   <v-footer
     dark
-    height="auto"
+    height="55"
   >
     <v-card
       class="flex"
@@ -10,12 +10,21 @@
     >
       <v-card-title class="teal">
         <strong class="subheading">LOGO </strong>
-
         <v-spacer></v-spacer>
+    <v-btn fab dark small color="indigo">
+      <v-icon dark>account_circle</v-icon>
+    </v-btn>
 
-       
       </v-card-title>
 
     </v-card>
   </v-footer>
 </template>
+<script>
+  export default {
+    data: () => ({
+      dropdown_font: ['Arial', 'Calibri', 'Courier', 'Verdana'],
+   
+    })
+  }
+</script>
